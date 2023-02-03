553fec1d8feaa69d5c727779cf358a302cb822ea
risalkn <risalknklm@gmail.com>
2023-02-03 20:15:15 +0000

    inital commit

---
+++ src/main/java/task/management/TaskManagementApp/TaskManagementAppApplication.java
@@ -0,0 +1,13 @@
+package task.management.TaskManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/task/management/TaskManagementApp/TaskManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package task.management.TaskManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
